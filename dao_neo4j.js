27d5c9a56d8c19f3ec67f4f43abc604d61706479
senetale <alex.senet38@gmail.com>
2022-02-22 08:41:36 +0100

    Removed test function

--- dao_neo4j.js
+++ dao_neo4j.js
@@ -30,24 +30,3 @@ class Dao_Neo4j{
 }
 
 module.exports = Dao_Neo4j
-
-async function toto() {
-
-    try {
-        const result = await session.run(
-            'CREATE (a:Person {name: $name}) RETURN a',
-            {name: personName}
-        )
-
-        const singleRecord = result.records[0]
-        const node = singleRecord.get(0)
-
-        console.log(node.properties.name)
-    } finally {
-        await session.close()
-    }
-
-// on application exit:
-    await driver.close()
-
-}
